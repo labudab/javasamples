317f9091d7cc6b366cd2b56ec9a34727d8832196
Borislav <Borislav@a4-4000>
2015-03-08 13:58:25 +0300

    Collection

---
+++ proj13/src/proj13/Main.java
@@ -0,0 +1,19 @@
+package proj13;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+	private static ArrayList<Mobile> mobiles = new ArrayList<Mobile>();
+	private static Random r = new Random();
+
+	public static void main(String[] args) {
+		for(int i=0;i<300;i++){
+			mobiles.add(new Mobile(r.nextInt(99931239), "sasasasa"));
+		}
+		for (Mobile m: mobiles){
+			System.out.println(m.getNumber()+" "+m.getName());
+		}
+	}
+
+}
