7412beef215ebdc0eab7f1f8d5cdfcaaab7a5827
Borislav <Borislav@a4-4000>
2015-03-08 14:02:47 +0300

    Collectionssss

--- proj13/src/proj13/Main.java
+++ proj13/src/proj13/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		for(int i=0;i<300;i++){
-			mobiles.add(new Mobile(r.nextInt(99931239), "sasasasa"));
+			mobiles.add(new Mobile(r.nextInt(9993139), "sasasasa"));
 		}
 		for (Mobile m: mobiles){
 			System.out.println(m.getNumber()+" "+m.getName());
